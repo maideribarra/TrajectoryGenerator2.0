ac821984d269494730932acf9ac7ef1c755877fd
maideribarra <maideribarra@opendeusto.es>
2023-03-07 17:54:43 +0000

    mas graficas

--- TrajectoriesGenerator/model/Autoencoder LSTM v2/predict.py
+++ TrajectoriesGenerator/model/Autoencoder LSTM v2/predict.py
@@ -16,56 +16,105 @@ import os
 import yaml
 from matplotlib import pyplot as plt 
 
-def showOutput(predictions):
+x0 = -1.5
+x1 = 1.5
+y0 = -1
+y1 = 2
+
+arrVariables = ['x','y','vx','vy','angle','vangle','l0','l1','action','n_episodio']
+
+def showOutput(predictions, ruta, x, y):
     out = predictions[0][0][1]
     input = predictions[0][0][0].movedim(0,1)
     for i in range(1,10):
-        plt.plot(out[i][:][:,0],out[i][:][:,1])
-        plt.plot(input[i][:][:,0],input[i][:][:,1])
+        plt.plot(out[i][:][:,x],out[i][:][:,y])
+        plt.plot(input[i][:][:,x],input[i][:][:,y])
+        plt.title(arrVariables[x]+arrVariables[y])
+        plt.xlim(x0, x1)
+        plt.ylim(y0, y1)
+        plt.xlabel(arrVariables[x])
+        plt.ylabel(arrVariables[y])
+        plt.savefig(ruta+str(i)+arrVariables[x]+arrVariables[y]+'outBatchOne.png')
         plt.show()
+        
 
 
-def showAllOutputs(predictions):
+def showAllOutputs(predictions, ruta, x, y):
     for index, batch in enumerate(predictions):
         output=batch[0][1]
         #print('output shape',output.shape)
         for inner, traj in enumerate(output):
             #print('traj shape',traj.shape)
-            plt.plot(traj[:][:,0],traj[:][:,1])
+            plt.plot(traj[:][:,x],traj[:][:,y])
             #print(traj[:])
             #outputs.append(traj[:][:].numpy())
             #print(f"total: {index}/{len(predictions)-1} local: {inner}/{len(output)-1}", end="\r")
+    plt.xlim(x0, x1)
+    plt.ylim(y0, y1)
+    plt.xlabel(arrVariables[x])
+    plt.ylabel(arrVariables[y])
+    plt.title(arrVariables[x]+arrVariables[y])
+    plt.savefig(ruta+arrVariables[x]+arrVariables[y]+'allOutput.png')
     plt.show()  
+    
 
-def showAllInputs(predictions):
+def showAllInputs(predictions, ruta, x, y):
     for batch in predictions:
         output=batch[0][0].movedim(0,1)
         for traj in output:
-            plt.plot(traj[:][:,0],traj[:][:,1])
+            plt.plot(traj[:][:,x],traj[:][:,y])
             #print(traj[:])
             #outputs.append(traj[:][:].numpy())
+    plt.xlim(x0, x1)
+    plt.ylim(y0, y1)
+    plt.xlabel(arrVariables[x])
+    plt.ylabel(arrVariables[y])
+    plt.title(arrVariables[x]+arrVariables[y])
+    plt.savefig(ruta+arrVariables[x]+arrVariables[y]+'allInput.png')
     plt.show()    
+    
 
-def showAllOutputsBatchOne(predictions):
+def showAllOutputsBatchOne(predictions, ruta, x, y):
     for index, batch in enumerate(predictions):
         output=batch[0][1]
-        plt.plot(output[:][:,0],output[:][:,1])
+        plt.plot(output[:][:,x],output[:][:,y])
+    plt.xlim(x0, x1)
+    plt.ylim(y0, y1)
+    plt.xlabel(arrVariables[x])
+    plt.ylabel(arrVariables[y])
+    plt.title(arrVariables[x]+arrVariables[y])
+    plt.savefig(ruta+arrVariables[x]+arrVariables[y]+'allOutputBatchOne.png')
     plt.show()  
+    
 
-def showAllInputsBatchOne(predictions):
+def showAllInputsBatchOne(predictions, ruta, x,y):
     for batch in predictions:
         output=batch[0][0].movedim(0,1)
         #print('output shape',output.shape)
-        plt.plot(output[0][:][:,0],output[0][:][:,1])
+        plt.plot(output[0][:][:,x],output[0][:][:,y])
+    plt.xlim(x0, x1)
+    plt.ylim(y0, y1)
+    plt.xlabel(arrVariables[x])
+    plt.ylabel(arrVariables[y])
+    plt.title(arrVariables[x]+arrVariables[y])
+    plt.savefig(ruta+arrVariables[x]+arrVariables[y]+'allInputBatchOne.png')
     plt.show()    
+    
 
-def showOutputBatchOne(predictions):
+def showOutputBatchOne(predictions, ruta, x,y):
     for i in range(1,10):
         out = predictions[i][0][1]
         input = predictions[i][0][0].movedim(0,1)
-        plt.plot(out[:][:,0],out[:][:,1])
-        plt.plot(input[0][:][:,0],input[0][:][:,1])
+        plt.plot(out[:][:,x],out[:][:,y])
+        plt.plot(input[0][:][:,x],input[0][:][:,y])
+        plt.xlim(x0, x1)
+        plt.ylim(y0, y1)
+        plt.xlabel(arrVariables[x])
+        plt.ylabel(arrVariables[y])
+        plt.title(arrVariables[x]+arrVariables[y])
+        plt.savefig(ruta+str(i)+arrVariables[x]+arrVariables[y]+'outBatchOne.png')
         plt.show()
+        
 
 def showStatisticsOfDimensionBatchOne(predictions):
     res = []
@@ -95,8 +144,9 @@ if __name__ == "__main__":
     # First initialize our model.
     #Experimento para probar dataset de 40000, hidden layer=1000
     cwd = os.getcwd()
-    dir="/home/ubuntu/ws_acroba/src/shared/egia/TrajectoriesGenerator/model/Autoencoder LSTM v2/experimentos/exp9/"
-    with open(dir+"exp9.yaml", "rb") as f:
+    experimento=sys.argv[1]
+    dir='/home/ubuntu/ws_acroba/src/shared/egia/TrajectoriesGenerator/model/Autoencoder LSTM v2/experimentos/'+experimento+'/'+experimento+'.yaml'
+    with open(dir, "rb") as f:
         datos = yaml.load(f, yaml.Loader)
         NUM_SEQ = datos['NUM_SEQ']
         INPUT_DIM = datos['INPUT_DIM']
@@ -126,8 +176,7 @@ if __name__ == "__main__":
                      workdir + TEST_DATASET,
                      BATCH_SIZE,
                      NUM_SEQ)
-   
-    logdir = cwd +'/../'+ LOG_DIR
+    logdir = LOG_DIR
     logger = TensorBoardLogger(logdir, name="LSTM")
     num_gpus = 1 if torch.cuda.is_available() else 0
     chk_path = CHK_PATH
@@ -143,8 +192,29 @@ if __name__ == "__main__":
     print(len(predictions[0][0]))
     print(len(predictions[0][0][0]))
     print(len(predictions[0][0][0][0]))
-    showOutputBatchOne(predictions)
-    showAllInputsBatchOne(predictions)
-    showAllOutputsBatchOne(predictions)
+    #Trayectorias
+    showOutputBatchOne(predictions, dir,0,1)
+    showAllInputsBatchOne(predictions, dir,0,1)
+    showAllOutputsBatchOne(predictions, dir,0,1)
+
+    showOutputBatchOne(predictions, dir,0,2)
+    showAllInputsBatchOne(predictions, dir,0,2)
+    showAllOutputsBatchOne(predictions, dir,0,2)
+
+    showOutputBatchOne(predictions, dir,1,3)
+    showAllInputsBatchOne(predictions, dir,1,3)
+    showAllOutputsBatchOne(predictions, dir,1,3)
+
+    showOutputBatchOne(predictions, dir,4,5)
+    showAllInputsBatchOne(predictions, dir,4,5)
+    showAllOutputsBatchOne(predictions, dir,4,5)
+
+    showOutputBatchOne(predictions, dir,1,6)
+    showAllInputsBatchOne(predictions, dir,1,6)
+    showAllOutputsBatchOne(predictions, dir,1,6)
+
+    showOutputBatchOne(predictions, dir,1,7)
+    showAllInputsBatchOne(predictions, dir,1,7)
+    showAllOutputsBatchOne(predictions, dir,1,7)
     showStatisticsOfDimensionBatchOne(predictions)
     
